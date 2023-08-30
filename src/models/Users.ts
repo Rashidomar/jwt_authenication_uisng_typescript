b10d280f450b4647fe1db7c5e1d3d544a53567b1
Rashidomar <amidurashid@gmail.com>
2023-08-30 17:37:46 +0000

    fix: fixed in a problem in the models folder

---
+++ src/models/Users.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+
+// enum UserTypes {
+//     Pharmacy,
+//     Newsletter,
+//     Magazine,
+//     Book
+//   }
+
+interface IUser {
+    name: string;
+    email: string;
+    // userType : UserTypes,
+    password : string,
+  }
+
+
+const schemaUser = new Schema<IUser>({
+
+    name: { 
+        type: String, 
+        required: true
+    },
+    email: { 
+        type: String, 
+        required: true,
+        unique: true 
+    },
+    password: { 
+        type: String,
+        required: true,
+
+    },
+});
+
+export let User = model<IUser>('User', schemaUser)
+
+// export default {User}
