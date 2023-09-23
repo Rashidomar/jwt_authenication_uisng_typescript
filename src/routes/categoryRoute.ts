9ad8e47f403e392d1ca8bc4336dd3c589dc5bddd
Rashidomar <amidurashid@gmail.com>
2023-09-23 11:36:14 +0000

    Fix; fixed an issue in the categoryRoute

--- src/routes/categoryRoute.ts
+++ src/routes/categoryRoute.ts
@@ -3,12 +3,12 @@ import { addCategory, getCategories, getCategory, updateCategory, deleteCategory
 import { createCategorySchema, updateCategorySchema } from "../schema/categorySchema";
 import { validate } from "../middleware/validateInputs";
 
-const Router = express.Router()
+const router = express.Router()
 
-Router.get("/get_category/:categoryId", getCategory)
-Router.get("/get_allcategories", getCategories)
-Router.post("/add_category", validate(createCategorySchema), addCategory)
-Router.put("edit_category/:categoryId", validate(updateCategorySchema), updateCategory)
-Router.delete("delete_category/:categoryId", deleteCategory)
+router.get("/get_category/:categoryId", getCategory)
+router.get("/get_allcategories", getCategories)
+router.post("/add_category", validate(createCategorySchema), addCategory)
+router.put("edit_category/:categoryId", validate(updateCategorySchema), updateCategory)
+router.delete("delete_category/:categoryId", deleteCategory)
 
-export default Router
+export default router
